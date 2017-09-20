0e9e59e71d32770b86dbfce5d2eeccec300204e3
sdevine188 <sdevine188@gmail.com>
2017-09-20 10:18:14 -0400

    updated with magick package

--- tesseract_scratchpad.R
+++ tesseract_scratchpad.R
@@ -1,6 +1,9 @@
 # https://www.r-bloggers.com/the-new-tesseract-package-high-quality-ocr-in-r/
+# https://cran.r-project.org/web/packages/magick/vignettes/intro.html
 
 # install.packages("tesseract")
+# install.packages("magick")
+library(magick)
 library(tesseract)
 library(stringr)
 
@@ -34,4 +37,40 @@ cat(folder_text)
 cat(folder_text[1])
 cat(folder_text[2])
 
+# test handwritten form
+getwd()
+text <- ocr("C:/Users/Stephen/Desktop/R/tesseract/test_docs/handwritten_form.png")
+cat(text)
 
+# test handwritten form - high quality
+# doesnt load picture for some reason??
+text <- ocr("C:/Users/Stephen/Desktop/R/tesseract/test_docs/handwritten_form_high_quality.png")
+cat(text)
+
+
+####################################
+
+
+# test using magick
+
+# receipt
+img <- image_read("C:/Users/Stephen/Desktop/ocr_test/ocr_test_receipt.png")
+print(img)
+cat(image_ocr(img))
+
+# birth certificate
+img <- image_read("C:/Users/Stephen/Desktop/ocr_test/ocr_test_birth_certificate.png")
+print(img)
+cat(image_ocr(img))
+
+# low-quality handwritten
+handwritten2 <- image_read("C:/Users/Stephen/Desktop/R/tesseract/test_docs/handwritten_form.png")
+print(handwritten2)
+handwritten2 <- image_ocr(handwritten2)
+cat(handwritten2)
+
+# high-quality handwritten
+handwritten_hq2 <- image_read("C:/Users/Stephen/Desktop/R/tesseract/test_docs/handwritten_form_high_quality.png")
+print(handwritten_hq2)
+handwritten_hq2 <- image_ocr(handwritten_hq2)
+cat(handwritten_hq2)
